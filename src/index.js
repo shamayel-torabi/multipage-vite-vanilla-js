01e7ad84671fc9919507708e159fd6ce798dbb08
Shamayel Torabi <sh-torabi@hrec.co.ir>
2025-04-20 10:14:40 +0330

    first commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { setupHeader } from "@js/components/header.js";
+import { setupCounter } from "@js/components/counter";
+
+setupHeader(document.querySelector("#header"), "ویدئو");
+setupCounter(document.querySelector("#counter"));
